af761b93a314d2e6bebb41209fe0d415c6c49981
aimee attenborough <aimeelattenborough@gmail.com>
2018-03-17 13:17:14 +0000

    places sidebar is scrollable

--- src/controllers/trips/tripsIndex.js
+++ src/controllers/trips/tripsIndex.js
@@ -1,6 +1,6 @@
-TripsIndexCtrl.$inject = [];
+TripsIndexCtrl.$inject = ['$http'];
 
-function TripsIndexCtrl() {
+function TripsIndexCtrl($http) {
 
   const vm = this; //ViewModel allows us to use this in function
   vm.isActive = true;
@@ -17,9 +17,14 @@ function TripsIndexCtrl() {
     console.log(vm.showModal);
     if(vm.form.$invalid) return false;
   }
-
-
   this.handleSubmit = handleSubmit;
+
+  vm.all = {};
+  $http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&key=AIzaSyAPEmf9_jhooXaFG-9VI-W1catLwZ5vGfg')
+    .then(res => Object.assign(vm.all, res))
+    .then(res => vm.all = res.data);
+  return console.log(vm.all);
+
 
 
 }
